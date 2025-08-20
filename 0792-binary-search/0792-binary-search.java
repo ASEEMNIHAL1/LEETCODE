e85a7a590ef8767aacb99fda701349b88b2fee69
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2025-08-20 22:06:29 +0530

    Time: 0 ms (100%), Space: 45.7 MB (78.35%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -9,21 +9,33 @@ class Solution {
         // return -1;
 
         int low = 0, high = nums.length - 1;
+        // while(low <= high) {
+        //     int mid = low + (high - low) / 2;
+        //     if(nums[mid] == target) return mid;
+        //     if(nums[low] <= nums[mid]) {
+        //         if(target >= nums[low] && target < nums[mid]) {
+        //             high = mid - 1;
+        //         } else {
+        //             low = mid + 1;
+        //         }
+        //     } else {
+        //         if(target > nums[mid] && target <= nums[high]) {
+        //             low = mid + 1;
+        //         } else {
+        //             high = mid - 1;
+        //         }
+        //     }
+        // }
+        // return -1;
+
         while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid]) {
-                if(target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else {
-                if(target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }else if(target > nums[mid]) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
             }
         }
         return -1;
