b8013b2d277a9a6369483cc409a4a544485848a1
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2025-09-05 22:57:18 +0530

    Time: 1 ms (38.46%), Space: 41 MB (44.62%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.java
@@ -1,22 +1,15 @@
 class Solution {
-    
-    // Method to count set bits in a long number
-    private int countSetBits(long n) {
-        int count = 0;
-        while (n > 0) {
-            count += n & 1;
-            n >>= 1;
-        }
-        return count;
-    }
-
     public int makeTheIntegerZero(int num1, int num2) {
-        int ops = 1;
-        while (true) {
-            long val = (long) num1 - (long) ops * num2;
-            if (val < 0) return -1;
-            if (countSetBits(val) <= ops && ops <= val) return ops;
-            ops++;
+        for(int i = 1; ; i++) {
+            long val = (long)num1 - (long)i * num2;
+
+            if(val < 0) {
+                return -1;
+            }
+
+            if(Long.bitCount(val) <= i && i <= val) {
+                return (int)i;
+            }
         }
     }
 }
