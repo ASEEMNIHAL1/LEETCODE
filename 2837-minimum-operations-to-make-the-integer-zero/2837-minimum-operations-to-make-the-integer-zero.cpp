d44193d67eb27453bda90ad79e509e66e56493ec
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2025-09-05 22:25:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.57%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    int countSetBits(long long n) {
+        int count = 0;
+        while (n) {
+            count += n & 1; // Add 1 if the last bit is set
+            n >>= 1;        // Right shift by 1
+        }
+        return count;
+    }
+
     int makeTheIntegerZero(int num1, int num2) {
         int ops = 1;
         while(true){
             long long val = (long long)num1 - (long long)ops*num2;
             if (val<0) return -1;
-            if( __builtin_popcountll(val) <= ops && ops<=val) return ops;
+            if(countSetBits(val) <= ops && ops<=val) return ops;
             ops++;
             
         }
