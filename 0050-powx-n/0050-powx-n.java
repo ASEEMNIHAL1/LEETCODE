f438d174d8b340641efcd91ded6cad9e6d9b460e
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2026-03-07 17:53:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res = Math.pow(x,n);
-        return res;
+        // double res = Math.pow(x,n);
+        // return res;
+
+        long N = n;
+        if(N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x , N);
+    }
+
+    double fastPow(double x, long n) {
+        if(n == 0) return 1.0;
+
+        double half = fastPow(x , n / 2);
+
+        if(n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
     }
 }
