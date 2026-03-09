ec2a7975722c40101249c2225096af194510109a
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2026-03-09 23:03:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,36 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int freq[] = new int[n+1];
+        // int n = nums.length;
+        // int freq[] = new int[n+1];
+        // for(int val : nums) {
+        //     freq[val]++;
+        // }
+
+        // int a = 0, b = 0;
+
+        // for(int i = 0 ; i <= n; i++){
+        //     if(freq[i] == 2) {
+        //         a = i;
+        //     } else if(freq[i] == 0) {
+        //         b = i;
+        //     }
+        // }
+        // return new int[]{a,b};
+
+        Map<Integer,Integer> map = new HashMap<>();
         for(int val : nums) {
-            freq[val]++;
+            map.put(val,map.getOrDefault(val, 0) + 1);
         }
 
-        int a = 0, b = 0;
+        int missing = 0 , duplicate = 0;
 
-        for(int i = 0 ; i <= n; i++){
-            if(freq[i] == 2) {
-                a = i;
-            } else if(freq[i] == 0) {
-                b = i;
+        for(int i = 1; i <= nums.length; i++) {
+            if(!map.containsKey(i)) {
+                missing = i;
+            } else if( map.get(i) == 2) {
+                duplicate = i;
             }
         }
-        return new int[]{a,b};
+        return new int[]{duplicate,missing};
     }
 }
