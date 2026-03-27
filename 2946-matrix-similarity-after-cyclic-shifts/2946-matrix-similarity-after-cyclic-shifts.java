61d2c75abde059134f3100e1de81bf8616534b35
AseemNihal <109451116+ASEEMNIHAL1@users.noreply.github.com>
2026-03-27 12:12:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -5,24 +5,33 @@ class Solution {
         int effective = k % n;
         if(effective == 0) return true;
 
-        int[][] matrix = new int[m][n];
+        // int[][] matrix = new int[m][n];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n ; j++) {
-                int newPos;
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n ; j++) {
+        //         int newPos;
 
-                if (i % 2 == 0) {
-                    newPos = (j - effective + n) % n; 
-                } else {
-                    newPos = (j + effective) % n;
-                }
-                matrix[i][newPos] = mat[i][j];
-            }
-        }
+        //         if (i % 2 == 0) {
+        //             newPos = (j - effective + n) % n; 
+        //         } else {
+        //             newPos = (j + effective) % n;
+        //         }
+        //         matrix[i][newPos] = mat[i][j];
+        //     }
+        // }
+
+        // for(int i = 0; i < m; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         if(mat[i][j] != matrix[i][j]) return false;
+        //     }
+        // }
+        // return true;
 
         for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] != matrix[i][j]) return false;
+            for(int j = 0; j < n ; j++) {
+                if(mat[i][j] != mat[i][(j - effective + n)%n]) {
+                    return false;
+                }
             }
         }
         return true;
